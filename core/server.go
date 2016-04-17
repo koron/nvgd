5b60026f09518a0ccfff3823912be8e7a47889d3
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-04-17 21:56:11 +0900

    filter core implemented
    fix #3

--- core/server.go
+++ core/server.go
@@ -1,10 +1,14 @@
 package core
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 
+	"github.com/koron/nvgd/filter"
 	"github.com/koron/nvgd/protocol"
 )
 
@@ -35,22 +39,103 @@ func (s *Server) Run() error {
 	return s.httpd.ListenAndServe()
 }
 
-func (s *Server) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+func (s *Server) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	path := req.URL.Path[1:]
 	u, err := url.Parse(path)
 	if err != nil {
-		s.log.Printf("failed to parse %q as URL: %s", path, err)
 		// TODO: error response
+		s.log.Printf("failed to parse %q as URL: %s", path, err)
 		return
 	}
 	p := protocol.Find(u.Scheme)
 	if p == nil {
-		s.log.Printf("not found protocol for %q", u.Scheme)
 		// TODO: error response
+		s.log.Printf("not found protocol for %q", u.Scheme)
 		return
 	}
 	r, err := p.Open(u.Path)
-	// TODO: fix me.
+	if err != nil {
+		// TODO: error response
+		s.log.Printf("failed to open: %s", path)
+		return
+	}
+	r, err = s.applyFilters(u.RawQuery, r)
+	if err != nil {
+		if r != nil {
+			r.Close()
+		}
+		// TODO: error response
+		s.log.Printf("filter error: %s", err)
+		return
+	}
 	defer r.Close()
+	// TODO: better log
 	s.log.Printf("%s %s", req.Method, req.URL.Path)
+	res.WriteHeader(http.StatusOK)
+	_, err = io.Copy(res, r)
+}
+
+func (s *Server) applyFilters(q string, r io.ReadCloser) (io.ReadCloser, error) {
+	for q != "" {
+		k := q
+		if i := strings.Index(k, "&"); i >= 0 {
+			k, q = k[:i], k[i+1:]
+		} else {
+			q = ""
+		}
+		if k == "" {
+			continue
+		}
+		v := ""
+		if i := strings.Index(k, "="); i >= 0 {
+			k, v = k[:i], k[i+1:]
+		}
+		k, err := url.QueryUnescape(k)
+		if err != nil {
+			return r, err
+		}
+		v, err = url.QueryUnescape(v)
+		if err != nil {
+			return r, err
+		}
+		r2, err := s.applyFilter(k, v, r)
+		if err != nil {
+			return r, err
+		}
+		r = r2
+	}
+	return r, nil
+}
+
+func (s *Server) applyFilter(name, params string, r io.ReadCloser) (io.ReadCloser, error) {
+	f := filter.Find(name)
+	if f == nil {
+		return nil, fmt.Errorf("not found filter: %s", name)
+	}
+	p, err := s.parseParams(params)
+	if err != nil {
+		return nil, err
+	}
+	return f.Filter(r, p)
+}
+
+func (s *Server) parseParams(q string) (map[string]string, error) {
+	p := map[string]string{}
+	for q != "" {
+		k := q
+		if i := strings.Index(k, ";"); i >= 0 {
+			k, q = k[:i], k[i+1:]
+		} else {
+			q = ""
+		}
+		if k == "" {
+			continue
+		}
+		v := ""
+		if i := strings.Index(k, ":"); i >= 0 {
+			k, v = k[:i], k[i+1:]
+		}
+		p[k] = v
+	}
+	return p, nil
 }
---
+++ filter/filter.go
@@ -0,0 +1,38 @@
+package filter
+
+import (
+	"fmt"
+	"io"
+)
+
+// Filter is abstraction of methods of filtering.
+type Filter interface {
+	Filter(io.ReadCloser, map[string]string) (io.ReadCloser, error)
+}
+
+var filters = map[string]Filter{}
+
+// Register registers a filter with name.
+func Register(name string, f Filter) error {
+	_, ok := filters[name]
+	if ok {
+		return fmt.Errorf("duplicated filter name %q", name)
+	}
+	return nil
+}
+
+// MustRegister registers a filter, panic if failed.
+func MustRegister(name string, f Filter) {
+	if err := Register(name, f); err != nil {
+		panic(err)
+	}
+}
+
+// Find finds a filter.
+func Find(name string) Filter {
+	f, ok := filters[name]
+	if !ok {
+		return nil
+	}
+	return f
+}
