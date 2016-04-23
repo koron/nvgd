a9d56e847e56a75d1f447d1e09a616a90f04aabb
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-04-23 23:31:34 +0900

    LTSV filter, complete
    fix #13

--- filter/ltsv.go
+++ filter/ltsv.go
@@ -7,6 +7,48 @@ import (
 	"strings"
 )
 
+type ltsv map[string][]string
+
+func parseLTSV(s string) ltsv {
+	r := ltsv{}
+	for _, raw := range strings.Split(s, "\t") {
+		kv := strings.SplitN(raw, ":", 2)
+		if len(kv) != 2 {
+			continue
+		}
+		k, v := kv[0], kv[1]
+		slot, _ := r[k]
+		r[k] = append(slot, v)
+	}
+	return r
+}
+
+func (v ltsv) put(buf *bytes.Buffer) error {
+	first := true
+	for k, slot := range v {
+		for _, v := range slot {
+			if first {
+				first = false
+			} else {
+				if _, err := buf.WriteString("\t"); err != nil {
+					return err
+				}
+			}
+			if _, err := buf.WriteString(k); err != nil {
+				return err
+			}
+			if _, err := buf.WriteString(":"); err != nil {
+				return err
+			}
+			if _, err := buf.WriteString(v); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// LTSV represents a structure for LTSV (labeled tab separated value)
 type LTSV struct {
 	Base
 	label string
@@ -15,6 +57,7 @@ type LTSV struct {
 	cut   []string
 }
 
+// NewLTSV creates a new instance of LTSV.
 func NewLTSV(r io.ReadCloser, label string, re *regexp.Regexp, match bool, cut []string) *LTSV {
 	l := &LTSV{
 		label: label,
@@ -27,8 +70,53 @@ func NewLTSV(r io.ReadCloser, label string, re *regexp.Regexp, match bool, cut [
 }
 
 func (l *LTSV) readNext(buf *bytes.Buffer) error {
-	// TODO:
-	return io.EOF
+	for {
+		b, err := l.ReadLine()
+		if err != nil {
+			return err
+		}
+		v := parseLTSV(string(b))
+		if !l.isMatch(v) {
+			continue
+		}
+		if err := l.filter(v).put(buf); err != nil {
+			return err
+		}
+		if _, err := buf.WriteString("\n"); err != nil {
+			return err
+		}
+	}
+}
+
+func (l *LTSV) isMatch(v ltsv) bool {
+	if l.label == "" {
+		return true
+	}
+	slot, ok := v[l.label]
+	if !ok {
+		return false
+	}
+	for _, v := range slot {
+		if l.re.MatchString(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func (l *LTSV) filter(v ltsv) ltsv {
+	if len(l.cut) == 0 {
+		return v
+	}
+	r := ltsv{}
+	for _, label := range l.cut {
+		slot, ok := v[label]
+		if !ok {
+			continue
+		}
+		r[label] = slot
+	}
+	return r
 }
 
 func newLTSV(r io.ReadCloser, p Params) (io.ReadCloser, error) {
