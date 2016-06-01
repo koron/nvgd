d61351f5ef2f8d123c010577019124bd4207872e
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-06-02 07:46:39 +0900

    lz4 test

--- protocol/file_test.go
+++ protocol/file_test.go
@@ -36,3 +36,19 @@ func TestGzip(t *testing.T) {
 		t.Errorf("content of \"testdata/file_test.gz\" is unexpected: %q", s)
 	}
 }
+
+func TestLZ4(t *testing.T) {
+	f := &File{}
+	r, err := f.openFile("testdata/file_test.lz4")
+	if err != nil {
+		t.Fatal(err)
+	}
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	s := (string)(b)
+	if s != "this is lz4 compressed" {
+		t.Errorf("content of \"testdata/file_test.lz4\" is unexpected: %q", s)
+	}
+}
