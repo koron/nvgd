2aa59c5e3f1f8e9d9cc1373fe3804ac21f969307
MURAOKA Taro <koron.kaoriya@gmail.com>
2017-02-15 09:48:46 +0900

    default_filters in config

--- config/config_test.go
+++ config/config_test.go
@@ -1,6 +1,10 @@
 package config
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/koron/nvgd/internal/assert"
+)
 
 func TestDefault(t *testing.T) {
 	c, err := LoadConfig("empty.yml")
@@ -31,5 +35,28 @@ func TestOnlyAddr(t *testing.T) {
 	}
 	if c.ErrorLogPath != defaultErrorLog {
 		t.Errorf("ErrorLogPath should be %q but %q", defaultErrorLog, c.ErrorLogPath)
+	}
+}
+
+func TestOnlyFilters(t *testing.T) {
+	c, err := LoadConfig("filters.yml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(c.Filters) != 2 {
+		t.Fatal("default_filters should have 2 items")
+	}
+	const (
+		k1 = "/file:///var/"
+		k2 = "/file:///tmp/"
+		k3 = "/file:///unknown/"
+	)
+	v1, _ := c.Filters[k1]
+	assert.Equals(t, v1, Filters{"tail"}, "for path %q", k1)
+	v2, _ := c.Filters[k2]
+	assert.Equals(t, v2, Filters{"head", "tail=limit:5"}, "for path %q", k2)
+	_, v3 := c.Filters[k3]
+	if v3 {
+		t.Error("Filters should be zero for unknown path")
 	}
 }
---
+++ config/filters.go
@@ -0,0 +1,7 @@
+package config
+
+// Filters is definition of filters.
+type Filters []string
+
+// FiltersMap is map string to Filters.
+type FiltersMap map[string]Filters
