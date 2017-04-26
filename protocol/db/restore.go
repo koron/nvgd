856d0539262c4c2a343e8b9c4411ab2594750b59
MURAOKA Taro <koron.kaoriya@gmail.com>
2017-04-26 12:30:16 +0900

    parse XLSX data

--- protocol/db/restore.go
+++ protocol/db/restore.go
@@ -3,10 +3,13 @@ package db
 import (
 	"errors"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/url"
 
 	"github.com/koron/nvgd/protocol"
 	"github.com/koron/nvgd/resource"
+	"github.com/tealeg/xlsx"
 )
 
 type RestoreHandler struct {
@@ -21,5 +24,24 @@ func (rh *RestoreHandler) Open(u *url.URL) (*resource.Resource, error) {
 }
 
 func (rh *RestoreHandler) Post(u *url.URL, r io.Reader) (*resource.Resource, error) {
+	xf, err := rh.openXLSX(r)
+	if err != nil {
+		return nil, err
+	}
+	for _, sh := range xf.Sheets {
+		log.Printf("Sheet: %q", sh.Name)
+	}
 	return nil, errors.New("RestoreHandler#Post: not implemented yet")
+}
+
+func (rh *RestoreHandler) openXLSX(r io.Reader) (*xlsx.File, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	xf, err := xlsx.OpenBinary(b)
+	if err != nil {
+		return nil, err
+	}
+	return xf, nil
 }
