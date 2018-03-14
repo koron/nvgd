42512bbffa62246dd71bf0e92bb25a29682016ff
MURAOKA Taro <koron.kaoriya@gmail.com>
2018-03-14 12:45:48 +0900

    '-pprofaddr' to start net/http/pprof server

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
+	_ "net/http/pprof"
 
 	"github.com/koron/nvgd/config"
 	"github.com/koron/nvgd/core"
@@ -10,6 +12,7 @@ import (
 
 var (
 	configOpt = flag.String("c", "nvgd.conf.yml", "configuration file")
+	pprofAddr = flag.String("pprofaddr", "", "address for pprof server")
 )
 
 func main() {
@@ -17,6 +20,12 @@ func main() {
 	c, err := config.LoadConfig(*configOpt)
 	if err != nil {
 		log.Fatalf("failed to load config: %s", err)
+	}
+	if *pprofAddr != "" {
+		log.Printf("start pprof on %s", *pprofAddr)
+		go func() {
+			log.Println(http.ListenAndServe(*pprofAddr, nil))
+		}()
 	}
 	if err := core.Run(c); err != nil {
 		log.Fatalf("failed to run server: %s", err)
