9760b2b82a2b453da93629e835ab8b14f2f46371
MURAOKA Taro <koron.kaoriya@gmail.com>
2018-06-13 12:25:46 +0900

    `-version` option to show version

---
+++ internal/version/version.go
@@ -0,0 +1,5 @@
+package version
+
+const (
+	Version = "1.0.0"
+)
--- main.go
+++ main.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
 
 	"github.com/koron/nvgd/config"
 	"github.com/koron/nvgd/core"
+	"github.com/koron/nvgd/internal/version"
 )
 
 var (
 	configOpt = flag.String("c", "nvgd.conf.yml", "configuration file")
 	pprofAddr = flag.String("pprofaddr", "", "address for pprof server")
+	verFlag   = flag.Bool("version", false, "show version")
 )
 
 func main() {
 	flag.Parse()
+	if *verFlag {
+		fmt.Println(version.Version)
+		return
+	}
 	c, err := config.LoadConfig(*configOpt)
 	if err != nil {
 		log.Fatalf("failed to load config: %s", err)
