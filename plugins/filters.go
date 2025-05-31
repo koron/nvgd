65e0f8dd2ead2650cab514166f362f352640471b
MURAOKA Taro <koron.kaoriya@gmail.com>
2025-05-31 20:20:40 +0900

    new filter: "cutline" implemented

--- plugins/filters.go
+++ plugins/filters.go
@@ -3,6 +3,7 @@ package plugins
 import (
 	_ "github.com/koron/nvgd/filter/count"
 	_ "github.com/koron/nvgd/filter/cut"
+	_ "github.com/koron/nvgd/filter/cutline"
 	_ "github.com/koron/nvgd/filter/echarts"
 	_ "github.com/koron/nvgd/filter/grep"
 	_ "github.com/koron/nvgd/filter/hash"
