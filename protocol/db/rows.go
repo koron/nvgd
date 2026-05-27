e9b44f88186574bc1ec45f75a2f6957cbf0d7ec5
MURAOKA Taro <koron.kaoriya@gmail.com>
2026-05-27 19:31:38 +0900

    fix: scan DB columns as any/[]byte instead of sql.NullString to prevent binary data corruption

--- protocol/db/rows.go
+++ protocol/db/rows.go
@@ -3,6 +3,7 @@ package db
 import (
 	"bytes"
 	"database/sql"
+	"fmt"
 	"io"
 
 	"github.com/koron/nvgd/internal/ltsv"
@@ -24,7 +25,7 @@ func rows2ltsv(rows *sql.Rows, maxRows int) (io.ReadCloser, bool, error) {
 
 	vals := make([]any, n)
 	for i := range vals {
-		vals[i] = new(sql.NullString)
+		vals[i] = new(any)
 	}
 	strs := make([]string, n)
 
@@ -35,11 +36,14 @@ func rows2ltsv(rows *sql.Rows, maxRows int) (io.ReadCloser, bool, error) {
 			return nil, false, err
 		}
 		for i, v := range vals {
-			ns := v.(*sql.NullString)
-			if ns.Valid {
-				strs[i] = ns.String
-			} else {
+			p := v.(*any)
+			switch val := (*p).(type) {
+			case nil:
 				strs[i] = NullReplacement
+			case []byte:
+				strs[i] = string(val)
+			default:
+				strs[i] = fmt.Sprint(val)
 			}
 		}
 		w.Write(strs...)
